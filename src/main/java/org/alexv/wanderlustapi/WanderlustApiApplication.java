9975ffcab55e0dd2747a8ca644a1fe813fdbcf13
alex <varaticalexandru1@gmail.com>
2024-03-10 19:24:26 +0200

    project init

---
+++ src/main/java/org/alexv/wanderlustapi/WanderlustApiApplication.java
@@ -0,0 +1,13 @@
+package org.alexv.wanderlustapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderlustApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderlustApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alexv/wanderlustapi/WanderlustApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alexv.wanderlustapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderlustApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
