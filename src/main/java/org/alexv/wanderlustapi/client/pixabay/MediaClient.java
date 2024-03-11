a98962d3979e1b5693e5b4a7fa120e75d232f5be
alex <varaticalexandru1@gmail.com>
2024-03-11 10:23:47 +0200

    add Media [Feign] Client: - add getMedias() mapping;

---
+++ src/main/java/org/alexv/wanderlustapi/client/pixabay/MediaClient.java
@@ -0,0 +1,18 @@
+package org.alexv.wanderlustapi.client.pixabay;
+
+import org.alexv.wanderlustapi.api.dto.pixabay.MediasDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "pixabay-media", configuration = PixabayFeignConfig.class, url = "${pixabay.media.fetch.url}")
+public interface MediaClient {
+    @GetMapping()
+    MediasDto getMedias(
+            @RequestParam("q") String query,
+            @RequestParam("image_type") String imageType,
+            @RequestParam("orientation") String orientation,
+            @RequestParam("category") String category,
+            @RequestParam("per_page") Integer perPage
+    );
+}
