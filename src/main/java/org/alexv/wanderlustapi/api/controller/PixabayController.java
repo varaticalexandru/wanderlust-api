3adf9d91cf93fa5c76b51c6138f8d82992514a3b
alex <varaticalexandru1@gmail.com>
2024-03-11 10:24:21 +0200

    add PixabayControllert: - add fetchMedias() mapping;

---
+++ src/main/java/org/alexv/wanderlustapi/api/controller/PixabayController.java
@@ -0,0 +1,36 @@
+package org.alexv.wanderlustapi.api.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.alexv.wanderlustapi.api.dto.pixabay.MediasDto;
+import org.alexv.wanderlustapi.client.pixabay.MediaClient;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/pixabay")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class PixabayController {
+
+    MediaClient mediaClient;
+
+    public static final String FETCH_MEDIAS = "/media";
+
+    @GetMapping(FETCH_MEDIAS)
+    public MediasDto fetchMedias(
+            @RequestParam("q") String query,
+            @RequestParam("image_type") String imageType,
+            @RequestParam("orientation") String orientation,
+            @RequestParam("category") String category,
+            @RequestParam("per_page") Integer perPage
+    ) {
+        return mediaClient.getMedias(
+                query,
+                imageType,
+                orientation,
+                category,
+                perPage
+        );
+    }
+}
