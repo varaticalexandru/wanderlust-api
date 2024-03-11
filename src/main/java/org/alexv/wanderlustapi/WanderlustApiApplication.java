a312755fe060d414ce6746b553a5d29ff4a81cfe
alex <varaticalexandru1@gmail.com>
2024-03-11 10:19:40 +0200

    upd WanderlustApiApplication: - removed unused import;

--- src/main/java/org/alexv/wanderlustapi/WanderlustApiApplication.java
+++ src/main/java/org/alexv/wanderlustapi/WanderlustApiApplication.java
@@ -4,7 +4,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableFeignClients
