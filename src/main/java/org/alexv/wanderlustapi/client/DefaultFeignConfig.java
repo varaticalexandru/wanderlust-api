0bec14e89ddc3e812c5e139a3f540d04bead759f
alex <varaticalexandru1@gmail.com>
2024-03-11 10:22:41 +0200

    add DefaultFeignConfig: - configure OkHttp as underlying http client; - set Feign logging level to basic;

---
+++ src/main/java/org/alexv/wanderlustapi/client/DefaultFeignConfig.java
@@ -0,0 +1,21 @@
+package org.alexv.wanderlustapi.client;
+
+import feign.Logger;
+import feign.okhttp.OkHttpClient;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Bean;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class DefaultFeignConfig {
+
+    @Bean
+    public OkHttpClient client() {
+        return new OkHttpClient();
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
+}
