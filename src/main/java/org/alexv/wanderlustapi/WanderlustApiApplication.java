99fba8b83a24c90b9b167344f1a0cba67ee0381d
alex <varaticalexandru1@gmail.com>
2024-03-11 10:18:55 +0200

    upd WanderlustApiApplication: - add static loadEnvVars() methods; - invoked env vars loading before loading any Spring context;

--- src/main/java/org/alexv/wanderlustapi/WanderlustApiApplication.java
+++ src/main/java/org/alexv/wanderlustapi/WanderlustApiApplication.java
@@ -1,13 +1,22 @@
 package org.alexv.wanderlustapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@EnableFeignClients
 public class WanderlustApiApplication {
 
 	public static void main(String[] args) {
+		laodEnvVars();
 		SpringApplication.run(WanderlustApiApplication.class, args);
 	}
 
+	public static void laodEnvVars() {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+	}
 }
