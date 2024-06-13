2dce9895fde94805f54a2d816a57eb1e130d39c8
alex <varaticalexandru1@gmail.com>
2024-06-13 13:31:42 +0300

    upd media (pixabay) feign client: adapt url

--- src/main/java/org/alexv/wanderlustapi/client/pixabay/MediaClient.java
+++ src/main/java/org/alexv/wanderlustapi/client/pixabay/MediaClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "pixabay-media", configuration = PixabayFeignConfig.class, url = "${pixabay.media.fetch.url}")
+@FeignClient(name = "pixabay-media", configuration = PixabayFeignConfig.class, url = "${pixabay-api.urls.media}")
 public interface MediaClient {
     @GetMapping()
     MediasDto getMedias(
