f0a78cd3444adec7291f859f96ab5a2976277bc2
alex <varaticalexandru1@gmail.com>
2024-06-13 13:32:18 +0300

    add openai controller with endpoint for fetching recommendations

---
+++ src/main/java/org/alexv/wanderlustapi/api/controller/openai/OpenaiController.java
@@ -0,0 +1,35 @@
+package org.alexv.wanderlustapi.api.controller.openai;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.alexv.wanderlustapi.api.dto.openai.RecommendationsRequestDto;
+import org.alexv.wanderlustapi.api.dto.openai.RecommendationsResponseDto;
+import org.alexv.wanderlustapi.client.openai.RecommendationsClient;
+import org.alexv.wanderlustapi.service.openai.OpenaiService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class OpenaiController {
+
+    RecommendationsClient recommendationsClient;
+    OpenaiService openaiService;
+
+    public static final String FETCH_RECOMMENDATIONS = "/recommendations";
+
+    @PostMapping(FETCH_RECOMMENDATIONS)
+    public RecommendationsResponseDto getRecommendations(
+            @RequestBody RecommendationsRequestDto request
+    ) {
+
+        return openaiService.getRecommendations(request);
+    }
+}
