dc16cbf7ffd6e893a1749b5ad05c997f95dfba20
alex <varaticalexandru1@gmail.com>
2024-06-16 17:18:25 +0300

    upd WanderlustApiApplication: add mongo repos allowing annotation

--- src/main/java/org/alexv/wanderlustapi/WanderlustApiApplication.java
+++ src/main/java/org/alexv/wanderlustapi/WanderlustApiApplication.java
@@ -4,9 +4,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableMongoRepositories
 public class WanderlustApiApplication {
 
 	public static void main(String[] args) {
