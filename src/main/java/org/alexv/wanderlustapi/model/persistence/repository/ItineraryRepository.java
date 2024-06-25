c7bbd097eb696ebc21364338304fa9cc2c65675d
alex <varaticalexandru1@gmail.com>
2024-06-25 12:30:02 +0300

    upd ItineraryRepository: add findItinerariesByUserId, findItinerariesByUserEmail methods

--- src/main/java/org/alexv/wanderlustapi/model/persistence/repository/ItineraryRepository.java
+++ src/main/java/org/alexv/wanderlustapi/model/persistence/repository/ItineraryRepository.java
@@ -3,5 +3,10 @@ package org.alexv.wanderlustapi.model.persistence.repository;
 import org.alexv.wanderlustapi.model.persistence.entity.Itinerary;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ItineraryRepository extends MongoRepository<Itinerary, String> {
+
+    List<Itinerary> findItinerariesByUserEmail(String userEmail);
+    List<Itinerary> findItinerariesByUserId(String userId);
 }
